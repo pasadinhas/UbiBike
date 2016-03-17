a363f0767990124e8d38008a049da775042d4d8c
Strabox <pardal.pires@gmail.com>
2016-03-17 18:02:19 +0000

    Server project creation

---
+++ UbiBikeServer/src/main/java/pt/ulisboa/tecnico/cmu/UbiBikeServerApplication.java
@@ -0,0 +1,12 @@
+package pt.ulisboa.tecnico.cmu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbiBikeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbiBikeServerApplication.class, args);
+	}
+}
